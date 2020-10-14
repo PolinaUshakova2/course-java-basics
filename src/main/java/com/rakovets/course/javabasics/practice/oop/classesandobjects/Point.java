e86dc6e27aaae01eb731d56a526c29cd39345a1a
Polina Ushakova <polushakova@mail.ru>
2020-10-14 14:05:19 +0300

    feat: add solution for Point.

---
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Point.java
@@ -0,0 +1,24 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+
+public class Point {
+    protected double x;
+    protected double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getX() {
+        return this.x;
+    }
+    public double getY() {
+        return this.y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+}
