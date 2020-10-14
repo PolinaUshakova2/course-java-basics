d19339744653b3c8eb1655ac4d5743101f31320d
Polina Ushakova <polushakova@mail.ru>
2020-10-14 14:07:16 +0300

    feat: add test for Rectangle.

---
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/RectangleTest.java
@@ -0,0 +1,16 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Point firstPoint = new Point(10,20);
+        Point secondPoint = new Point(30,30);
+        Rectangle rectangle = new Rectangle(firstPoint, secondPoint);
+        System.out.printf("First Point: X = %f, Y = %f\nSecond Point: X = %f, Y = %f\n",
+                           firstPoint.x,
+                           firstPoint.y,
+                           secondPoint.x,
+                           secondPoint.y);
+        System.out.printf("Perimeter: %f\n", rectangle.getPerimeter());
+        System.out.printf("Area: %f", rectangle.getArea());
+    }
+}
