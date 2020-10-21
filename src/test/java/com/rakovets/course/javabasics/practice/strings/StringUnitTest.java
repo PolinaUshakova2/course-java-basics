1aaab9e05f9ea93a9596b4b0d471df664f4e641c
Polina Ushakova <polushakova@mail.ru>
2020-10-21 18:11:40 +0300

    feat: add tasts for tasks.

--- src/test/java/com/rakovets/course/javabasics/practice/strings/StringUnitTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/strings/StringUnitTest.java
@@ -37,12 +37,66 @@ public class StringUnitTest {
     }
 
     @Test
-    void getStringWithoutWhiteSpaceInUpperCase() {
+    void getStringWithoutWhiteSpaceInUpperCaseTest() {
         Assertions.assertEquals("SAN12", stringUnit.getStringWithoutWhiteSpaceInUpperCase(" san12 "));
         Assertions.assertEquals("241 A L", stringUnit.getStringWithoutWhiteSpaceInUpperCase("   241 A l"));
         Assertions.assertEquals("AAAA", stringUnit.getStringWithoutWhiteSpaceInUpperCase("aaaa"));
         Assertions.assertEquals("TT", stringUnit.getStringWithoutWhiteSpaceInUpperCase("TT"));
     }
 
+    @Test
+    void getSubstringOfStringTest() {
+        Assertions.assertEquals("0987654321", stringUnit.getSubstringOfString("12345678900987654321"));
+        Assertions.assertEquals("false", stringUnit.getSubstringOfString(" "));
+        Assertions.assertEquals("123456789012345678901234", stringUnit.getSubstringOfString("1234567890123456789012345678901234567890"));
+    }
+
+    @Test
+    void replaceSymbolTest() {
+        Assertions.assertEquals(":)", stringUnit.replaceSymbol(":("));
+        Assertions.assertEquals("11 :)", stringUnit.replaceSymbol("11 :("));
+        Assertions.assertEquals("", stringUnit.replaceSymbol(""));
+    }
+
+    @Test
+    void isStringStartWithTheWordTest() {
+        Assertions.assertEquals(false, stringUnit.isStringStartWithTheWord("1kkkk1 ", "1"));
+        Assertions.assertEquals(false, stringUnit.isStringStartWithTheWord("", "bj"));
+        Assertions.assertEquals(true, stringUnit.isStringStartWithTheWord(" 6 ", " "));
+        Assertions.assertEquals(true, stringUnit.isStringStartWithTheWord("good morning good", "good"));
+    }
+
+    @Test
+    void getCountOfVowelsTest() {
+        Assertions.assertEquals(2, stringUnit.getCountOfVowels("Good"));
+        Assertions.assertEquals(5, stringUnit.getCountOfVowels("aaAAA"));
+        Assertions.assertEquals(0, stringUnit.getCountOfVowels(""));
+    }
+
+    @Test
+    void getCountOfPunctuationMarksTest() {
+        Assertions.assertEquals(3, stringUnit.getCountOfPunctuationMarks("!! . "));
+        Assertions.assertEquals(2, stringUnit.getCountOfPunctuationMarks("aa,,AAA"));
+        Assertions.assertEquals(0, stringUnit.getCountOfPunctuationMarks(""));
+    }
+
+    @Test
+    void getStringReversTest() {
+        Assertions.assertEquals("11", stringUnit.getStringRevers("11"));
+        Assertions.assertEquals("abba", stringUnit.getStringRevers("abba"));
+    }
+
+    @Test
+    void getAbbreviationOfFIO() {
+        Assertions.assertEquals("У.П.И", stringUnit.getAbbreviationOfFIO("Ушакова Полина игоревна"));
+        Assertions.assertEquals("С.К.И", stringUnit.getAbbreviationOfFIO("симанович катерина игоревна"));
+    }
+
+    @Test
+    void getStringOfNumbers() {
+        Assertions.assertEquals("2332", stringUnit.getStringOfNumbers("Уf2332f"));
+        Assertions.assertEquals("", stringUnit.getStringOfNumbers("  "));
+        Assertions.assertEquals("3", stringUnit.getStringOfNumbers(" 3 "));
+    }
 
 }
