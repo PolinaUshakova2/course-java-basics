a905211d049a6ee718e5da23c0b8d6268762ae94
Polina Ushakova <polushakova@mail.ru>
2020-10-30 14:11:02 +0300

    feat: add class Student.

---
+++ src/main/java/com/rakovets/course/javabasics/practice/jcf/Student.java
@@ -0,0 +1,48 @@
+package com.rakovets.course.javabasics.practice.jcf;
+
+public class Student {
+    private String name;
+    private String surname;
+    private int age;
+    private double averageAnnualMark;
+
+    public Student(String name, String surname, int age, double averageAnnualMark) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.averageAnnualMark = averageAnnualMark;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getAverageAnnualMark() {
+        return averageAnnualMark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAverageAnnualMark(double averageAnnualMark) {
+        this.averageAnnualMark = averageAnnualMark;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
+
