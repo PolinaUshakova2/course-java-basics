1ab685626b58c1c79fbe7071b7e7e264cda2b446
Polina Ushakova <polushakova@mail.ru>
2020-10-31 20:18:56 +0300

    feat: fixed class Car.

---
+++ src/main/java/com/rakovets/course/javabasics/practice/jcf/car/Ford.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.javabasics.practice.jcf.car;
+
+public class Ford extends Car {
+    public Ford(String brand, String model, String color, int yearManufacture, String registrationNumber) {
+        super(brand, model, color, yearManufacture, registrationNumber);
+    }
+}
